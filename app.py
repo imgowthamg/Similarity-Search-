dc807aa7fe5c0fc21a20fd66b9f26bb4c8864c58
Gowtham G <119653141+imgowthamg@users.noreply.github.com>
2024-04-12 16:33:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,31 +22,13 @@ def calculate_similarity_api(text1, text2):
     # Return the similarity score
     return similarity_score.item()
     
-@app.route('/handle_request', methods=['POST'])
-def handle_request():
-    # Parse the request body
-    try:
-        request_data = request.get_json()
-        text1 = request_data.get("text1")
-        text2 = request_data.get("text2")
-        
-        if text1 and text2:
-            # Calculate the similarity score
-            similarity_score = calculate_similarity_api(text1, text2)
-            response = {"similarity score": result}
-            return jsonify(response)
-            # Return the similarity score as a JSON response
+@app.route('/main', methods=['POST'])# Parse the request body
 
-        else:
-            st.error("Both text1 and text2 are required in the request body.")
-    except Exception as e:
-        st.error(f"Error processing request: {e}")
 
 def main():
-    # Define the Streamlit app title
+
     st.title("Semantic Textual Similarity (STS)")
 
-    handle_request()
 
     # User interface for inputting text1 and text2
     text1 = st.text_area("Enter the first paragraph (text1):")
@@ -62,7 +44,24 @@ def main():
             st.write(f"Similarity score: {similarity_score:.4f}")
         else:
             st.warning("Please enter both sentences to calculate similarity.")
+    try:
+        request_data = request.get_json()
+        text1 = request_data.get("text1")
+        text2 = request_data.get("text2")
+        
+        if text1 and text2:
+            # Calculate the similarity score
+            similarity_score = calculate_similarity_api(text1, text2)
+            response = {"similarity score": result}
+            return jsonify(response)
+            
+            # Return the similarity score as a JSON response
 
+        else:
+            st.error("Both text1 and text2 are required in the request body.")
+    except Exception as e:
+        st.error(f"Error processing request: {e}")
+    # Define the Streamlit app title
 if __name__ == "__main__":
     main()
 
