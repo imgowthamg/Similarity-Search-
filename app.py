a27608da7dce1f780ad5e797d5858dec2154a3c1
Gowtham G <119653141+imgowthamg@users.noreply.github.com>
2024-04-12 16:39:22 +0530

    Update app.py
    output final

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 
 import streamlit as st
 from sentence_transformers import SentenceTransformer, util
-from flask import Flask, request, jsonify
-import json
-app = Flask(__name__)
+
 
 # Load the SentenceTransformer model
 model = SentenceTransformer("all-MiniLM-L6-v2")
@@ -21,8 +19,6 @@ def calculate_similarity_api(text1, text2):
 
     # Return the similarity score
     return similarity_score.item()
-    
-@app.route('/main', methods=['POST'])# Parse the request body
 
 
 def main():
@@ -44,23 +40,7 @@ def main():
             st.write(f"Similarity score: {similarity_score:.4f}")
         else:
             st.warning("Please enter both sentences to calculate similarity.")
-    try:
-        request_data = request.get_json()
-        text1 = request_data.get("text1")
-        text2 = request_data.get("text2")
-        
-        if text1 and text2:
-            # Calculate the similarity score
-            similarity_score = calculate_similarity_api(text1, text2)
-            response = {"similarity score": result}
-            return jsonify(response)
-            
-            # Return the similarity score as a JSON response
 
-        else:
-            st.error("Both text1 and text2 are required in the request body.")
-    except Exception as e:
-        st.error(f"Error processing request: {e}")
     # Define the Streamlit app title
 if __name__ == "__main__":
     main()
